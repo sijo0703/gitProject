2cf912550f54b3b5aaacf6c3039d2ac346abcf9b
Sijoy Thomas <zjoy@pop-os.localdomain>
2024-09-24 17:54:46 -0500

    Added to test branch

--- first.py
+++ first.py
@@ -5,3 +5,11 @@ print("Upgraded Git 1")
 print("Upgraded Git 2")
 
 print("Added to a new branch")
+
+print("Sijoy's Git")
+
+print("Upgraded Git 1")
+
+print("Upgraded Git 2")
+
+print("Added a new branch test")
